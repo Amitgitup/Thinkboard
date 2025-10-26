1f2fc7f4c6297056dc4f2ed32f838a036fb5a106
Amitgitup <amitksingh3022@gmail.com>
2025-10-26 19:56:22 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
-
 
 dotenv.config();
 
@@ -13,11 +13,14 @@ console.log(process.env.MONGO_URI);
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 
 // middleware
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
 app.use(express.json());    // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);   
 
@@ -30,6 +33,14 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/thinkboard/dist")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","thinkboard","dist","index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
